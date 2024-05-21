6096c2e31ef1dc811268127218db2b2d91da1f5b
Jacob Casas <43353267+jcasas5253@users.noreply.github.com>
2024-05-21 15:37:27 -0700

    Update server.mjs

--- server.mjs
+++ server.mjs
@@ -9,6 +9,9 @@ const port = process.env.PORT || 3000;
 // Define API key
 const apiKey = process.env.NASA_API_KEY;
 
+// Serve static files from the "public" directory
+app.use(express.static('public'));
+
 // Set up a route to fetch NEO data
 app.get('/neo-data', async (req, res) => {
     const baseUrl = 'https://api.nasa.gov/neo/rest/v1/feed?api_key=';
@@ -25,6 +28,11 @@ app.get('/neo-data', async (req, res) => {
         console.error('Error fetching NEO data:', error);
         res.status(500).send('Internal Server Error');
     }
+});
+
+// Route to serve index.html with NASA_API_KEY embedded
+app.get('/', (req, res) => {
+    res.sendFile('index.html', { root: 'public', NASA_API_KEY: apiKey });
 });
 
 // Start the server
