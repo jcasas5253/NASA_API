8acb3fee00cf5644ec20d11e996b4101f42a46a4
Jacob Casas <skedabble@gmail.com>
2024-05-22 14:24:14 -0700

    moved listner

--- public/script.js
+++ public/script.js
@@ -118,6 +118,21 @@ toggleDataBtn.addEventListener('click', async () => {
   isButtonClicked = !isButtonClicked;
 });
 
+const getSpaceNewsBtn = document.getElementById('getSpaceNewsBtn');
+const newsCard = document.getElementById('news-card');
+
+getSpaceNewsBtn.addEventListener('click', async () => {
+    // Call the function to fetch space news when the getSpaceNewsBtn is clicked
+    await getSpaceNews();
+
+    // Toggle the height of the card to 'auto' after fetching space news
+    if (newsCard.style.height === '300px') {
+        newsCard.style.height = 'auto';
+    } else {
+        newsCard.style.height = '300px';
+    }
+});
+
 const getSpaceNews = async () => {
   try {
     const response = await fetch('/space-news');
@@ -218,14 +233,3 @@ function smoothScrollTo(targetY, duration) {
 }
 
 const newsCard = document.getElementById('news-card');
-
-getSpaceNewsBtn.addEventListener('click', () => {
-    // Toggle the height of the card to 'auto'
-    if (newsCard.style.height === '300px') {
-        newsCard.style.height = 'auto';
-    } else {
-        newsCard.style.height = '300px';
-    }
-});
-
-
