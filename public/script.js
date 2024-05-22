027878af0914eeb793a734e144a5f34aa7c58350
Jacob Casas <skedabble@gmail.com>
2024-05-22 10:03:12 -0700

    simplified scroll function and html edit

--- public/script.js
+++ public/script.js
@@ -157,31 +157,3 @@ function scrollToDescription() {
 
     smoothScrollTo(targetY, 1000); // Adjust duration for desired scroll speed
 }
-
-function smoothScrollTo(targetY, duration) {
-    const startY = window.scrollY;
-    const distance = targetY - startY;
-    let startTime = null;
-
-    function step(currentTime) {
-        if (!startTime) {
-            startTime = currentTime;
-        }
-
-        const timeElapsed = currentTime - startTime;
-        const progress = Math.min(timeElapsed / duration, 1); // Ensure progress does not exceed 1
-
-        window.scrollTo(0, startY + distance * easeInOutQuad(progress));
-
-        if (progress < 1) {
-            requestAnimationFrame(step);
-        }
-    }
-
-    // Easing function for a smooth scroll effect
-    function easeInOutQuad(t) {
-        return t < 0.5 ? 2 * t * t : -1 + (4 - 2 * t) * t;
-    }
-
-    requestAnimationFrame(step);
-}
